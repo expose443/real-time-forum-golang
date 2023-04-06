cdac9acda72c2887486776e6bec472c70c0dc371
abdu0222 <tanatar-abdurahman@mail.ru>
2023-04-06 16:49:50 +0600

    add auth

--- backend-api/internal/handlers/auth.go
+++ backend-api/internal/handlers/auth.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/expose443/real-time-forum-golang/backend-api/internal/models"
@@ -14,9 +15,42 @@ func (c *Client) Login(w http.ResponseWriter, r *http.Request) {
 		c.Logger.Error.Print(err)
 		return
 	}
-
-	c.Logger.Debug.Print(credintails)
+	user, err := c.AuthService.IsValidUser(credintails.Identifier, credintails.Password)
+	if err != nil {
+		c.Logger.Error.Print(err)
+		w.WriteHeader(400)
+		return
+	}
+	w.WriteHeader(200)
+	fmt.Fprintf(w, "hello %s", user.FirstName)
 }
 
 func (c *Client) Register(w http.ResponseWriter, r *http.Request) {
+	var user models.UserRegister
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		c.Logger.Error.Print(err)
+		return
+	}
+	if user.Password != user.ConfirmPassword {
+		c.Logger.Debug.Print("password doesn't match")
+		w.WriteHeader(400)
+		return
+	}
+
+	err = c.AuthService.CreateUser(&models.User{
+		Nickname:  user.Nickname,
+		Age:       user.Age,
+		Gender:    user.Gender,
+		FirstName: user.FirstName,
+		LastName:  user.LastName,
+		Email:     user.Email,
+		Password:  user.Password,
+	})
+	if err != nil {
+		c.Logger.Error.Print(err)
+		w.WriteHeader(400)
+		return
+	}
+	w.WriteHeader(200)
 }
--- backend-api/internal/service/auth_service.go
+++ backend-api/internal/service/auth_service.go
@@ -1,8 +1,19 @@
 package service
 
-import "github.com/expose443/real-time-forum-golang/backend-api/internal/repository"
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strconv"
 
-type AuthService interface{}
+	"github.com/expose443/real-time-forum-golang/backend-api/internal/models"
+	"github.com/expose443/real-time-forum-golang/backend-api/internal/repository"
+)
+
+type AuthService interface {
+	IsValidUser(identifier any, password string) (models.User, error)
+	CreateUser(user *models.User) error
+}
 
 func NewAuthService(dao repository.DAO) AuthService {
 	return &authService{
@@ -12,4 +23,46 @@ func NewAuthService(dao repository.DAO) AuthService {
 
 type authService struct {
 	userRepo repository.UserRepository
+}
+
+func (a *authService) IsValidUser(identifier any, password string) (models.User, error) {
+	t := reflect.TypeOf(identifier)
+
+	switch t {
+	case reflect.TypeOf(""):
+		user, err := a.userRepo.GetUserByIdentifier(fmt.Sprintf("%v", identifier))
+		if err != nil {
+			return models.User{}, err
+		}
+		if user.Password != password {
+			return models.User{}, errors.New("invalid password")
+		}
+		return user, nil
+
+	case reflect.TypeOf(0):
+		num, _ := strconv.Atoi(fmt.Sprintf("%v", identifier))
+		user, err := a.userRepo.GetUserByUserID(num)
+		if err != nil {
+			return models.User{}, err
+		}
+		if user.Password != password {
+			return models.User{}, errors.New("invalid password")
+		}
+		return user, nil
+	default:
+		return models.User{}, errors.New("invalid type of identifier")
+
+	}
+}
+
+func (a *authService) CreateUser(user *models.User) error {
+	_, err := a.userRepo.GetUserByIdentifier(user.Email)
+	if err == nil {
+		return errors.New("user exists")
+	}
+	_, err = a.userRepo.GetUserByIdentifier(user.FirstName)
+	if err == nil {
+		return errors.New("user exists")
+	}
+	return a.userRepo.CreateUser(user)
 }
